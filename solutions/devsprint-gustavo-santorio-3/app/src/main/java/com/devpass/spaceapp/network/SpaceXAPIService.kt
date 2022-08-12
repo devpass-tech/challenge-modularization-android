78595b510ed34be5d153ec61294ce675775b2cd5
Thays <43557124+spolaorthays@users.noreply.github.com>
2022-08-12 17:54:48 -0300

    Modulo Rocket Details (#4)
    * Migrado rocketdetails para novo modulo
    
    * Finalizada a migração do rocketdetails para novo modulo

--- solutions/devsprint-gustavo-santorio-3/app/src/main/java/com/devpass/spaceapp/network/SpaceXAPIService.kt
+++ solutions/devsprint-gustavo-santorio-3/app/src/main/java/com/devpass/spaceapp/network/SpaceXAPIService.kt
@@ -2,19 +2,14 @@ package com.devpass.spaceapp.network
 
 import com.devpass.spaceapp.launchlist.domain.LaunchDTO
 import com.devpass.spaceapp.launchlist.domain.LaunchesDTO
-import com.devpass.spaceapp.rocketdetails.domain.dto.RocketDetailDTO
 import retrofit2.http.GET
 import retrofit2.http.Path
-
 import retrofit2.http.POST
 
 interface SpaceXAPIService {
 
     @GET("/v5/launches/{id}")
     suspend fun fetchLaunch(@Path("id") id: String) : LaunchDTO
-
-    @GET("/v4/rockets/{id}")
-    suspend fun fetchDetailRocket(@Path("id") idRocket: String) : RocketDetailDTO
 
     @POST("/v5/launches/query")
     suspend fun fetchLaunches() : LaunchesDTO
